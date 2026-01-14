66634d1c4d49fd79bbe6159ffe5d3744c37620a2
Omer Zafar <omerzafar69@gmail.com>
2026-01-14 17:10:33 +0500

    Logout test case Fix

--- Locators/logout_page_locators.py
+++ Locators/logout_page_locators.py
@@ -1,23 +1,24 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 class LogoutPage:
     def __init__(self, driver):
         self.driver = driver
 
-        # Keep your locators for now (but absolute xpath is fragile)
+        # Locators
         self.profile = (By.XPATH, "/html/body/div[1]/div/div/div[3]/div[2]/header/div/div/div[2]/div[2]/button/h5")
-        self.logout = (By.XPATH, "/html/body/div[1]/div/div/div[3]/div[2]/header/div/div/div[2]/div[2]/div/ul/li[2]/div")
+        self.logout_btn = (By.XPATH, "/html/body/div[1]/div/div/div[3]/div[2]/header/div/div/div[2]/div[2]/div/ul/li[2]/div")
 
         # Login page element (proves we are logged out)
         self.email = (By.ID, "email")
 
     def logout(self):
         wait = WebDriverWait(self.driver, 10)
-
         wait.until(EC.element_to_be_clickable(self.profile)).click()
-        wait.until(EC.element_to_be_clickable(self.logout)).click()
+        time.sleep(2)
+        wait.until(EC.element_to_be_clickable(self.logout_btn)).click()
 
     def is_login_displayed(self):
         try:
@@ -27,3 +28,4 @@ class LogoutPage:
             return True
         except Exception:
             return False
+
--- Testcases/test_logout.py
+++ Testcases/test_logout.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from Config.config import Config
 from Locators.login_page_locators import LoginPage
 from Locators.logout_page_locators import LogoutPage
+import time
 
 def test_logout(driver):
     login_page = LoginPage(driver)
@@ -13,10 +14,12 @@ def test_logout(driver):
     # 2) Confirm dashboard is visible
     WebDriverWait(driver, 15).until(EC.visibility_of_element_located(Config.dashboard))
     assert login_page.is_dashboard_displayed(), "Login failed: Dashboard not displayed"
+    time.sleep(2)
 
     # 3) Logout
     logout_page = LogoutPage(driver)
     logout_page.logout()
+    time.sleep(2)
 
     # 4) Confirm returned to login page
     assert logout_page.is_login_displayed(), "Logout failed: Login page not displayed"
